b6f7c3d911dc7add6f20f170e70bc2d0c8f4bbe5
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-01-09 00:01:47 -0800

    modified:   index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
-        writeToFile(README.md, answers);
+        writeToFile(fileName, answers);
     })
 }
 
